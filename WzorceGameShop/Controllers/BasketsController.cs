0f26a34247feb70858820b1a5106d1b529a29920
Szymon Wojtala <szymonwojtala122@gmail.com>
2020-11-12 11:16:58 +0100

    New Controllers

---
+++ WzorceGameShop/Controllers/BasketsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WzorceGameShop.Data;
+using WzorceGameShop.Models;
+
+namespace WzorceGameShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+        private readonly GameShopContext _context;
+
+        public BasketsController(GameShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Baskets
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Basket>>> GetBaskets()
+        {
+            return await _context.Baskets.ToListAsync();
+        }
+
+        // GET: api/Baskets/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Basket>> GetBasket(int id)
+        {
+            var basket = await _context.Baskets.FindAsync(id);
+
+            if (basket == null)
+            {
+                return NotFound();
+            }
+
+            return basket;
+        }
+
+        // PUT: api/Baskets/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBasket(int id, Basket basket)
+        {
+            if (id != basket.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(basket).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BasketExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Baskets
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Basket>> PostBasket(Basket basket)
+        {
+            _context.Baskets.Add(basket);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBasket", new { id = basket.Id }, basket);
+        }
+
+        // DELETE: api/Baskets/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Basket>> DeleteBasket(int id)
+        {
+            var basket = await _context.Baskets.FindAsync(id);
+            if (basket == null)
+            {
+                return NotFound();
+            }
+
+            _context.Baskets.Remove(basket);
+            await _context.SaveChangesAsync();
+
+            return basket;
+        }
+
+        private bool BasketExists(int id)
+        {
+            return _context.Baskets.Any(e => e.Id == id);
+        }
+    }
+}
