eca248159608d4ea00505b266e8c24a92891eeb4
Szymon Wojtala <szymonwojtala122@gmail.com>
2021-01-10 17:06:09 +0100

    Edit for client

--- WzorceGameShop/Controllers/ClientsController.cs
+++ WzorceGameShop/Controllers/ClientsController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using WzorceGameShop.Data;
+using WzorceGameShop.Models;
 
 namespace WzorceGameShop.Controllers
 {
@@ -17,66 +19,56 @@ namespace WzorceGameShop.Controllers
             _context = context;
         }
 
-        public ActionResult GamesList()
+        public async Task<IActionResult> Edit(int? id)
         {
-            //var userGames = _context.ClientsGames.FirstOrDefault(x => x.ClientId == 1);
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return View(client);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult UpdateUserSaldo(IFormCollection collection)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Login,Saldo")] Client client)
         {
-            try
+            if (id != client.Id)
             {
-                return RedirectToAction(nameof(Index));
+                return NotFound();
             }
-            catch
+
+            if (ModelState.IsValid)
             {
-                return View();
+                try
+                {
+                    _context.Update(client);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClientExists(client.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index", "Games");
             }
+            return View(client);
         }
 
-        // GET: ClientsController/Edit/5
-        //public ActionResult Edit(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: ClientsController/Edit/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: ClientsController/Delete/5
-        //public ActionResult Delete(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: ClientsController/Delete/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Delete(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
+        private bool ClientExists(int id)
+        {
+            return _context.Clients.Any(e => e.Id == id);
+        }
     }
 }
